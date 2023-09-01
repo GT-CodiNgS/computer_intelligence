78286960f884b0dabe59289700dffe241acf1789
Gayas Thasmika <73925625+GT-CodiNgS@users.noreply.github.com>
2023-09-01 15:07:26 +0530

    predict function developed

--- api/main.py
+++ api/main.py
@@ -1,12 +1,52 @@
-from flask import Flask, request
 from flask_cors import CORS  # Import CORS from flask_cors
+from textblob import TextBlob
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle, re, nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
 
 app = Flask(__name__)
 CORS(app)  # Use CORS with the app
+with open("disClassifier.pkl", 'rb') as f:
+    classifier = pickle.load(f)
+with open("tfidf.pkl", 'rb') as f:
+    tfidf = pickle.load(f)
+lemmatizer = WordNetLemmatizer()
 
-@app.route('/hello', methods=['POST'])
-def hello_world():
-    return 'Hello, World!'
+
+def predict_result(text, lemmatizer, tfidf, classifier):
+    text = text.lower()
+    text = re.sub('#', '', text)
+    text = re.sub('[^a-zA-Z ]', '', text)
+    words = nltk.word_tokenize(text)
+    words = [lemmatizer.lemmatize(word) for word in words if word not in set(stopwords.words('english'))]
+    sentence = ' '.join(words)
+    temp = [sentence]
+    temp1 = tfidf.transform(temp)
+    result = classifier.predict(temp1)
+    return result[0]
+
+
+def get_label(result):
+    if result == 0:
+        return "Not Disaster"
+    elif result == 1:
+        return "Disaster"
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    text = request.form['tweet']
+    result = predict_result(text, lemmatizer, tfidf, classifier)
+    label = get_label(result)
+    return label
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
